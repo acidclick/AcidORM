312d129e727491534f6febea38fb5a21bef38c42
acidclick <me@acidclick.cz>
2015-03-29 15:58:32 +0200

    Added method for creating entites by single table

--- Generators/DB/Postgre.php
+++ Generators/DB/Postgre.php
@@ -32,35 +32,38 @@ class Postgre extends BaseDatabase implements Generators\IDatabaseFirst{
 
 	public function createFromTable($table)
 	{
-		$this->db->query('\d+ ' . $table);
+		$qc = $this->db->query('select * from [information_schema].[columns] where [table_name] = %s order by [ordinal_position]', $table);
+		$oid = $this->db->query('SELECT c.oid FROM pg_catalog.pg_class c WHERE c.relname = %s', $table)->fetch()->oid;
+		$properties = [];
+		foreach($qc as $qr){
+			$qa = $this->db->query('select pg_catalog.col_description(%i, %i) as comment', $oid, $qr->ordinal_position);
+			$property = new \StdClass;
+			$property->name = $qr->column_name;
+			$property->annotations = $this->getPropertyAnnotations($qa->fetch()->comment);
+			$properties[] = $property;
+		}
+
+		$qfk = $this->db->query('
+			SELECT obj_description(%sql::regclass) as note
+			FROM pg_class
+			WHERE relkind = %s limit 1
+		', '\'"' . $table . '"\'', 'r');
+
+		$note = $qfk->fetch()->note;
+		$dependencies = $this->getDependencies($note);	
+		$annotations = $this->getAnnotations($note);		
+
+		$this->createData($table, $properties, $dependencies, $annotations);
+		$this->createPersistor($table);
+		$this->createMapper($table);
+		$this->createFacade($table);
 	}
 
 	public function createAll()
 	{
 		$qt = $this->db->query('SELECT * FROM [pg_catalog].[pg_tables] where [schemaname] = %s', 'public');
 		foreach($qt as $rt){
-			$qc = $this->db->query('select * from [information_schema].[columns] where [table_name] = %s', $rt->tablename);
-
-			$properties = [];
-			foreach($qc as $qr){
-				$properties[] = (Object)['name' => $qr->column_name];
-			}
-			$properties = array_reverse($properties);
-
-			$qfk = $this->db->query('
-				SELECT obj_description(%sql::regclass) as note
-				FROM pg_class
-				WHERE relkind = %s limit 1
-			', '\'"' . $rt->tablename . '"\'', 'r');
-
-			$note = $qfk->fetch()->note;
-			$dependencies = $this->getDependencies($note);	
-			$annotations = $this->getAnnotations($note);		
-
-			$this->createData($rt->tablename, $properties, $dependencies, $annotations);
-			$this->createPersistor($rt->tablename);
-			$this->createMapper($rt->tablename);
-			$this->createFacade($rt->tablename);
+			$this->createFromTable($rt->tablename);
 		}
 		    
 	}
